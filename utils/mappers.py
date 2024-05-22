971fdb570a8355859b701181f70b6122d834f19d
zViniAlves <alve.schikman@gmail.com>
2024-05-22 19:48:35 -0300

    first commit :)

---
+++ utils/mappers.py
@@ -0,0 +1,26 @@
+import json
+
+def run(csv:list,index_col_key:int,index_col_value:int,i_mapper):
+    dict_map = {}
+    for row in csv[1:-1]:
+        row = row.split(";")
+
+        key = row[index_col_key]
+        value = row[index_col_value]
+
+        if key[0] == '"' and key[-1] == '"':
+            key = key[1:-1]
+
+        if value[0] == '"' and value[-1] == '"':
+            value = value[1:-1]
+
+        value = value.replace(',','.') if ',' in value else value
+
+        value = float(value)
+        if key in dict_map.keys():
+            dict_map[key].append(value)
+        else:
+            dict_map[key] = [value]
+
+    with open(f'temp/data_{i_mapper}.json','w') as out_file:
+        json.dump(dict_map,out_file)
---
+++ utils/reader.py
@@ -0,0 +1,14 @@
+def csv(path:str,col_key:str,col_value:str,**kwargs):
+    sep = ',' if not 'sep' in kwargs else kwargs['sep']
+
+    csv = open(path,'r').read().split('\n')
+    header = csv[0]
+
+    for col in [col_key,col_value]:
+        if not col in header:
+            raise KeyError(f'{col} not found in csv columns')
+    
+    index_col_key = header[:header.find(col_key)].count(sep)
+    index_col_value = header[:header.find(col_value)].count(sep)
+
+    return csv, index_col_key, index_col_value
